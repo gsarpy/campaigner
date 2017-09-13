38b85b1985e7d877d22ae8376edb33e47565793c
Greg Sarpy <evsoul@evair.local>
2017-09-13 15:43:31 -0700

    created procfile for heroku

--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app     = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    {
+      hi: 'there',
+      omg: 'totally',
+      good: 'i guess'
+    }
+  );
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> {
+  console.log("Express server running on port " + PORT);
+});
